c0f678319e95754ff723ed1f893da38d532f2b62
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2023-12-27 23:48:53 +0330

    reload add

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { InfoContext } from "../context/Provider";
 import { IoManSharp } from "react-icons/io5";
 import { IoWoman } from "react-icons/io5";
@@ -6,26 +6,40 @@ import styles from "../css/result.module.css";
 import { Link } from "react-router-dom";
 function Result() {
   const { save, next, setNext } = useContext(InfoContext);
+  const [reload, setReload] = useState(true);
+  if (save.length > 2) {
+    setTimeout(() => {
+      setReload(false);
+    }, 2000);
+  }
+
   console.log(save);
   return (
     <>
-      <ul className={styles.ul}>
-        {save.map((item) => (
-          <li className={styles.li} key={item.id}>
-            <p className={styles.icon}>
-              {item.gender === "man" ? (
-                <IoManSharp style={{ fontSize: `${item.height}px` }} />
-              ) : (
-                <IoWoman style={{ fontSize: `${item.height}px` }} />
-              )}
-            </p>
-            <div className={styles.nh}>
-              <p style={{ height: "1.5rem" }}>{item.name}</p>
-              <p style={{ height: "1.5rem" }}>{`${item.height} cm`}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {save.length > 2 ? (
+        <div>
+          <p>just two people can add</p>
+          {!reload ? window.location.reload() : null}
+        </div>
+      ) : (
+        <ul className={styles.ul}>
+          {save.map((item) => (
+            <li className={styles.li} key={item.id}>
+              <p className={styles.icon}>
+                {item.gender === "man" ? (
+                  <IoManSharp style={{ fontSize: `${item.height}px` }} />
+                ) : (
+                  <IoWoman style={{ fontSize: `${item.height}px` }} />
+                )}
+              </p>
+              <div className={styles.nh}>
+                <p style={{ height: "1.5rem" }}>{item.name}</p>
+                <p style={{ height: "1.5rem" }}>{`${item.height} cm`}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
